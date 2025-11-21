c462394fd223781f94721a56ad07efd7e462da6a
NAHOM WORKU HANKA <nahom.worku@sidamabanksc.com>
2025-11-21 16:33:57 +0300

    Fix the Verce Link Error

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
 });
 
 const corsOptions = {
-  origin: 'https://job-portal-beige-ten.vercel.app/',
+  origin: 'https://job-portal-beige-ten.vercel.app',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
