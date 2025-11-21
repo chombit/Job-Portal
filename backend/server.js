7b0f13a4e0c5aabe6b1ca0dbebdf8d708962df71
NAHOM WORKU HANKA <nahom.worku@sidamabanksc.com>
2025-11-21 16:25:20 +0300

    Set Vercel link to run backend

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
 });
 
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: 'https://job-portal-beige-ten.vercel.app/',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
